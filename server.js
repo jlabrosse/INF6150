79688b233293c0231248ac6faad21e5e016a0634
Nicolas Hamard <nox.tux@gmail.com>
2017-03-01 20:12:40 -0500

    Fonction d'aspirateur mappe sur server.js pour pouvoir tester les retours de gw2spidy

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 var express = require('express');
+var aspirateur = require("./lib/aspirateur.js");
 var server = express();
 
 server.set('port', (process.env.PORT || 8080));
 server.use(express.static(__dirname + '/public'));
 
-server.get('/index', function(req, res, cb){
+console.log(aspirateur);
+
+function reponsehttp(res, body) {
 	res.writeHeader(200, {"Content-type":"text/html"});
-	res.write("hello world, une petite niaiserie");
+	res.write(body);
 	res.end;
-	
+};
+
+
+server.get('/requete/tous', function(req, res, cb){
+	aspirateur.tousLesObjets(function(resultat){
+		reponsehttp(res, resultat);
+		return cb();
+	});
+});
+
+server.get('/requete/type/:type', function(req, res, cb){
+	aspirateur.parType(req.params.type, function(resultat){
+		reponsehttp(res, resultat);
+		return cb();
+	});
+});
+
+server.get('/requete/rarete/:rarete', function(req, res, cb){
+	aspirateur.parRarete(req.params.rarete, function(resultat){
+		reponsehttp(res, resultat);
+		return cb();
+	});
+});
+
+server.get('/requete/objet/:nom', function(req, res, cb){
+	aspirateur.obtenirObjet(req.params.nom, function(resultat){
+		reponsehttp(res, resultat);
+		return cb();
+	});
+});
+
+server.get('/index', function(req, res, cb){
+	reponsehttp(res, "Hello World");
 	return cb();
 });
 
